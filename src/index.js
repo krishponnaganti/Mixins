8e1437e3021f63b4de68efe4489e55c91545b554
RamaKrishna <64081163+krishponnaganti@users.noreply.github.com>
2021-09-18 16:34:54 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+const myMixin = {
+  name: "ganesh",
+  mobile: "9052040609"
+};
+function demoOne() {
+  this.tempFunc = () => {
+    console.log("this is temp function one.");
+  };
+}
+
+Object.assign(demoOne.prototype, myMixin);
+var newObj = new demoOne();
+
+// console.log("working... ");
+console.log(newObj.tempFunc());
+console.log(newObj.mobile);
